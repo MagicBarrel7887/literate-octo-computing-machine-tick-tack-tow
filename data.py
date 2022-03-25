b5cbe9394a99b4f99f6255c65588e9e3088babd2
nate <costerboy03@outlook.com>
2022-03-25 11:29:47 -0500

    loop search

--- data.py
+++ data.py
@@ -22,4 +22,53 @@ def displayBoard(board):
     print('+-------+-------+-------+')
 
 
-displayBoard(board)
+
+
+
+def enter_move(board):
+    """# The function accepts the board's current status, asks the user about their move,
+    # checks the input, and updates the board according to the user's decision."""
+    while True: 
+        move = int(input("Please enter a number in the board "))
+        if move < 1 or move > 9:
+            print('')
+            continue
+        elif str(move) not in board[0] and str(move) not in board[1] and str(move) not in board[2]:
+            print('')
+            continue
+        elif move == 1:
+            board[0][0]='0'
+        elif move == 2:
+            board[0][1] = '0'
+        elif move == 3:
+            board[0][2] = '0'
+        elif move == 4:
+            board[1][0]='0'
+        elif move == 5:
+            board[1][1]='0'
+        elif move == 6:
+            board[1][2]='0'
+        elif move == 7:
+            board[2][0] = '0'
+        elif move == 8:
+            board[2][1] = '0'
+        elif move == 9:
+            board[2][2] = '0'
+
+
+
+# def make_list_of_free_fields(board):
+#     # The function browses the board and builds a list of all the free squares;
+#     # the list consists of tuples, while each tuple is a pair of row and column numbers.
+
+
+# def victory_for(board, sign):
+#     # The function analyzes the board's status in order to check if
+#     # the player using 'O's or 'X's has won the game
+
+
+# def draw_move(board):
+
+while True:
+    displayBoard(board)
+    enter_move(board)
--- tick-tack_tow.py
+++ tick-tack_tow.py
@@ -1,9 +1,16 @@
+# The function draws the computer's move and updates the board.
+from distutils.log import error
+from pickle import FALSE
+import re
+
 """
 Provides some tick-tack_tow
+
 """
 
+temps = ''
 
-borad = [['1','2','3'], ['4','x','6'], ['7','8','9']]
+borad = [['1','2','3'], ['4','x','o'], ['7','8','9']]
 
 def display_data():
     """Provides some tick-tack_tow"""
@@ -11,13 +18,67 @@ def display_data():
         print('+-------+-------+-------+')
         print('|   ' + borad[i][0] + '   |   ' + borad[i][1] + '   |    ' + borad[i][2] + '  |')
         print('+-------+-------+-------+')
+
+
+def enter_move():
+    """ The function accepts the board's current status, asks the user about their move, checks the input, and updates the board according to the user's decision. """
+    
+    
+    try:
+      move = int(input("Please enter a number in the board "))
+      print(move)
+      
+      try:
+        if move < 1 or move > 9:
+            for x in range(0, 3):
+                for y in range(0, 2):
+                    temp = isinstance(borad[x][y], int)
+                    temps = temp
+                    print(temp, " ",x,":",y)
+                    
+                    if temp is False:
+                     print("Please enter a valed number in the board")
+                     print(temp)
+                     return False
+                       
+      except Exception as e:
+       print('loop search exception occurred', end="\n")
+       print(e)
         
-#     print('|   ' + borad[1][0] + '   |   ' + borad[1]
-#           [1] + '   |    ' + borad[1][2] + '  |     ')
-#     print('+-------+-------+-------+')
-#     print('|   ' + borad[2][0] + '   |   ' + borad[2]
-#           [1] + '   |    ' + borad[2][2] + '  |     ')
-#     print('+-------+-------+-------+')
+        
+       try:
+         for x in range(0, 3):
+            for y in range(0, 2):
+                if borad[x][y] == move:
+                    
+       except Exception as e:
+       print('An exception occurred')
+       print(e)
+      
+    except Exception as e:
+      print('An exception occurred')
+      print(e)
+    
+    
+    print()
 
 
-display_data()
+
+def make_list_of_free_fields():
+    """ The function browses the board and builds a list of all the free squares the list consists of tuples, while each tuple is a pair of row and column numbers."""
+    print()
+
+
+def victory_for(board, sign):
+    """ The function analyzes the board's status in order to check if the player using 'O's or 'X's has won the game"""
+    print()
+
+
+def draw_move(board):
+    """ The function draws the computer's move and updates the board."""
+    print()
+
+
+while True:
+    display_data()
+    enter_move()

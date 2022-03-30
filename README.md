dec7ba1796289809ee19e522983a18ef6f2aadb7
nate <costerboy03@outlook.com>
2022-03-30 11:13:47 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # literate-octo-computing-machine-tick-tack-tow
-a lab for tick-tack-tow in python
+a lab for tick-tack-tow in python and cpp
---
+++ cpp/tick tack tow/tick tack tow.cpp
@@ -0,0 +1,83 @@
+﻿// tick tack tow.cpp : Defines the entry point for the application.
+//
+
+#include "tick tack tow.h"
+
+using namespace std;
+
+char board_arry[3][3] = { {55,56,57},
+						 {52,53,54},
+						 {49,50,51}, };
+int in;
+
+int define_display_board() {
+	for (size_t y = 0; y < 3; y++)
+	{
+		for (size_t x = 0; x < 3; x++)
+		{
+
+			cout << " | " << board_arry[y][x] << " | ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+int get_user_input() {
+	cin >> in;
+
+	char c = '0' + in;
+
+	for (size_t y = 0; y < 3; y++)
+	{
+		for (size_t x = 0; x < 3; x++)
+		{
+			if (c == board_arry[y][x])
+			{
+				cout << x <<" : " << y << "   " << board_arry[y][x] << " true" << endl;
+				board_arry[y][x] = 'O';
+			}
+			else {
+				cout << x << " : " << y << "   " << board_arry[y][x] << " false" << endl;
+			}
+			
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
+int test() {
+	return 0;
+}
+
+bool borad_game_full(char d) {
+	for (size_t y = 0; y < 3; y++)
+	{
+		for (size_t x = 0; x < 3; x++)
+		{
+			if (d == board_arry[y][x])
+			{
+				return
+			}
+			else {
+				return
+			}
+
+		}
+		
+	}
+}
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	define_display_board();
+	while (true)
+	{
+		get_user_input();
+		test();
+		define_display_board();
+	}
+	return 0;
+}
